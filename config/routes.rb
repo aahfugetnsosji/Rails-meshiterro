edb5ae3f0c84960e59406847cafee3a3d583b379
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-03-23 16:28:37 +0000

    [Add] 4-11途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  # devise_for :post_images
+  devise_for :post_images
   devise_for :users
   
  resources :post_images, only: [:new, :create, :index, :show]
